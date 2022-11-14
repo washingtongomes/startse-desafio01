2740ab1774aebd144919082cdd81f02d3edda647
Washington Gomes <wsgomes600@gmail.com>
2022-11-14 11:50:47 -0300

    Confetes

--- myscript.js
+++ myscript.js
@@ -1,4 +1,6 @@
 
+
+
 function clickMenu(){
     if (items.style.display == "block"){
         items.style.display = "none"
@@ -10,5 +12,16 @@ function clickMenu(){
 }
 
 
+
+
 const nome = prompt ('Digite seu nome: ');
 document.querySelector('#id-nome').textContent = `Bem-vindo... ${nome} `
+
+
+
+var confettiSettings = {target: 'my-canvas'};
+var confetti = new ConfettiGenerator(confettiSettings);
+confetti.render();
+
+
+
